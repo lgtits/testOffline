93e7ed67c9fa99b03fd88a1e1316085ef0a1786b
a0937370704 <a0937370704@gmail.com>
2024-06-27 12:07:43 +0800

    test

---
+++ assets/i18n.93f3771f.js
@@ -0,0 +1,1 @@
+import{c as s}from"./index.125062c9.js";import{e as n,z as o,j as r}from"./index.e3208ede.js";var t={en:n,zh:o,ja:r},i=({app:a})=>{const e=s({legacy:!1,locale:"en",globalInjection:!0,messages:t});a.use(e)};export{i as default};
